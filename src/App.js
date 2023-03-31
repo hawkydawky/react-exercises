e7ef742c4b3100935e4bd7c5347c394959d992ee
hawkydawky <ahmetegeyilmaz@gmail.com>
2023-04-01 02:07:02 +0300

    conditional-rendering-02

--- src/App.js
+++ src/App.js
@@ -3,6 +3,6 @@ import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
     render() {
-        return <Welcome name=<strong>Bernard</strong> age={25} />
+        return <Welcome name=<strong>Bernard</strong> />
     }
 }
