58d422e0e56e512c0264bc67e0d306b38f6cf0ba
hawkydawky <ahmetegeyilmaz@gmail.com>
2023-04-01 04:37:23 +0300

    conditional-rendering-04

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,11 +5,9 @@ export class Welcome extends React.Component {
     render() {
         return (
             <div>
-                <p>Welcome, {this.props.name || "John"}!</p>
-                {this.props.age>18 && this.props.age<65 ? <Age age={this.props.age}/> : false}
+                <p>Welcome, {this.props.name || "John Doe"}!</p>
+                {this.props.age>18 && this.props.age<65 && this.props.name ==="John" ? <Age age={this.props.age}/> : false}
             </div>
         )
     }
 }
-
-// {this.props.age && <Age age={this.props.age}/>} also works but i wanted to use ternary operator
