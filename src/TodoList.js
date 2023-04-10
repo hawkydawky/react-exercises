4067b00d015edbc04ebfabf4143837febb2ed58f
hawkydawky <ahmetegeyilmaz@gmail.com>
2023-04-10 17:44:44 +0300

    lists-06

--- src/TodoList.js
+++ src/TodoList.js
@@ -30,10 +30,16 @@ export class TodoList extends React.Component {
         })
     }
 
+    removeTodo = (event) => {
+        this.setState((state) => ({
+            items: state.items.filter((item, index) => index !== Number(event.target.getAttribute("data-index")))
+        }))
+    }
+
     render() {
         return (
             <div>
-                <ul>{this.state.items.map((ele, index) => <li key={index}>{ele}</li>)}</ul>
+                <ul>{this.state.items.map((ele, index) => <li key={index}>{ele} <button data-index={index} onClick={this.removeTodo}>Remove</button> </li>)}</ul>
                 <input type="text" name="element" value={this.state.element} onChange={this.changeComponent}/>
                 <button onClick={this.addElement}>Add</button>
                 <button onClick={this.resetTodo}>Reset</button>
