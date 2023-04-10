1124497502c6265396f3297eb873666763a5b5b1
hawkydawky <ahmetegeyilmaz@gmail.com>
2023-04-10 03:56:54 +0300

    forms-06

--- src/UncontrolledLogin.js
+++ src/UncontrolledLogin.js
@@ -12,8 +12,8 @@ export class UncontrolledLogin extends React.Component {
         return (
             <div>
                 <form onSubmit={this.formSubmit}>
-                    <input type="text" name="username"/>
-                    <input type="password" name="password"/>
+                    <input type="text" name="username" autoFocus="true" placeholder="Username"/>
+                    <input type="password" name="password" placeholder="Password"/>
                     <input type="checkbox" name="isChecked"/>
                     <button type="submit">Login</button>
                     <button type="reset">Reset</button>
