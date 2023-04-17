83d8be31060bc41c656750c9b842212a0131935d
hawkydawky <ahmetegeyilmaz@gmail.com>
2023-04-17 16:03:16 +0300

    custom-hooks-04

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -2,7 +2,7 @@ import { useGithub } from "./useGithub"
 
 export function Githubuser() {
 
-    const {data, onFetch, onInputChange} = useGithub()
+    const {data, error,loading, onFetch, onInputChange} = useGithub()
 
     return (
         <div>
@@ -10,9 +10,14 @@ export function Githubuser() {
             <h1>Github Username Fetcher!</h1>
             <input type="text" name="usernameInput" placeholder="username" onChange={onInputChange}/>
             <button onClick={onFetch}>Fetch</button>
+            {loading && <h2>Loading...</h2>}
+            {error && <div>
+                <h3>There is an error!</h3>
+                <p>{error.message}</p>
+            </div>}
             {data && <h2>{data.name}</h2> }
             {data && <a href={data.html_url}>{data.html_url}</a> }
-            {data && <p>Public repo count: {data.public_repos}</p>}
+            {data && data.public_repos && <p>Public repo count: {data.public_repos}</p>}
         </div>
     )
 }
--- src/useGithub.js
+++ src/useGithub.js
@@ -3,12 +3,29 @@ import { useState } from "react"
 export function useGithub() {
     const [data, setData] = useState(null)
     const [usernameInput, setUsernameInput] = useState("")
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
 
     const handleFetch = async () => {
-        const response = await fetch(`https://api.github.com/users/${usernameInput}`)
-        const data = await response.json()
-        setData(data)
+        setLoading(true)
+        setError(null)
+        
+        try {
+          const response = await fetch(`https://api.github.com/users/${usernameInput}`)
+          const data = await response.json()
+          if (data.message) {
+            throw new Error(data.message)
+          }
+          setData(data)
+        } catch(e) {
+          setError(e)
+          setData(null)
+        } finally{
+            setLoading(false)
+        }
       }
+      
+      
     
 
     function handleUsernameInputChange(event) {
@@ -17,6 +34,8 @@ export function useGithub() {
 
     return {
         data: data,
+        error: error,
+        loading: loading,
         onFetch: handleFetch,
         onInputChange: handleUsernameInputChange
     }
