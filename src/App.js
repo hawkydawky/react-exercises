f06e4233a258c458db4c421c314012629df5c781
hawkydawky <ahmetegeyilmaz@gmail.com>
2023-04-26 06:53:24 +0300

    react-router-02

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import { Welcome } from './Welcome';
+import { Counter } from './Counter';
 
 export function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Welcome name="Bernard" />} />
+        <Route path='counter' element={<Counter />} />
       </Routes>
     </div>
   );
---
+++ src/Counter.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export function Counter({ initialValue = 0 }) {
+    const [count, setCount] = useState(initialValue);
+
+    function counter() {
+        setCount(current => current + 1);
+    }
+
+    function reset() {
+        setCount(initialValue);
+    }
+
+    return (
+        <div>
+            <h3>Count: {count}</h3>
+            <button onClick={counter}>Increase</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    )
+}
