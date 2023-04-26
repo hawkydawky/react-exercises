624ef1ff9f2ca7904697b2758e2d13008422f7b3
hawkydawky <ahmetegeyilmaz@gmail.com>
2023-04-26 06:49:28 +0300

    react-router-01

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import React from "react";
-import { Welcome } from "./Welcome";
-import { Login } from "./Login";
-import { LanguageContext } from "./LanguageContext";
-import { DisplayLanguage } from "./DisplayLanguage";
+import { Routes, Route } from 'react-router-dom';
+import { Welcome } from './Welcome';
 
-export class App extends React.Component {
-  state = {
-    language: 'en',
-  };
-
-  handleLanguageChange = (event) => {
-    this.setState({
-      language: event.target.value,
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <Welcome name="Bernard" />
-        <Login />
-        <LanguageContext.Provider value={this.state.language}>
-          <DisplayLanguage handleLanguageChange={this.handleLanguageChange} />
-        </LanguageContext.Provider>
-      </div>
-    );
-  }
+export function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Welcome name="Bernard" />} />
+      </Routes>
+    </div>
+  );
 }
